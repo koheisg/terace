4e7591eab226a44ca051d14678c4876fe4079a19
Kohei Sugi <kohei.sg@gmail.com>
2018-02-24 12:48:11 +0900

    Execute `$ rails db:migrate`
    ```
    == 20180224034613 CreateUsers: migrating ======================================
    -- create_table(:users)
       -> 0.0011s
    == 20180224034613 CreateUsers: migrated (0.0012s) =============================
    ```

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223172841) do
+ActiveRecord::Schema.define(version: 20180224034613) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
