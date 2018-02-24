c031880b342ff3081b66b60d3e32b46684f44ed7
Kohei Sugi <kohei.sg@gmail.com>
2018-02-24 12:45:13 +0900

    Delete articles#index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-  # GET /articles
-  # GET /articles.json
-  def index
-    @articles = Article.all
-  end
-
   # GET /articles/1
   # GET /articles/1.json
   def show
