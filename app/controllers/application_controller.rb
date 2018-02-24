f86460ca2dbf9bf0d5d04fa01d6c9ae463875cef
Kohei Sugi <kohei.sg@gmail.com>
2018-02-24 16:49:15 +0900

    Verify login user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :verify_user
+
+  def verify_user
+    redirect_to new_session_path unless current_user.persisted?
+  end
+
+  def current_user
+    User.find(session[:user_id])
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_user
   # GET /sessions/new
   def new
     @session = Session.new
