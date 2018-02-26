364ca91fc028c5b028cc08302bc02ed62d58934d
Kohei Sugi <kohei.sg@gmail.com>
2018-02-26 22:56:04 +0900

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  constraints(-> (req) { req.subdomain.present? }) do
-    mount Terrace::Engine => "/"
-  end
-
   constraints(-> (req) { %w[lvh.me terace.herokuapp.com].include?(req.host) }) do
     resources :sessions, only: [:new, :create, :destroy]
     resources :users
     resources :articles, expect: [:index, :show]
+  end
+
+  constraints(-> (req) { req.subdomain.present? }) do
+    mount Terrace::Engine => "/"
   end
 end
