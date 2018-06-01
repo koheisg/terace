de8b2c9b180b5a377927b87c549354e2f08d3c3b
Kohei Sugi <kohei.sg@gmail.com>
2018-06-01 11:40:35 +0900

    Add validate permalink

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,8 @@ class Article < ApplicationRecord
   has_many :taggings, as: :taggable
   has_many :tags, through: :taggings
 
+  validates :permalink, uniqueness: true, presence: true
+
   enum state: { draft: 0, published: 1 }
 
   def content_html
