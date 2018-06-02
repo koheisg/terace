97e5d776b250a4ebbcb8769475045423fbde637a
Kohei Sugi <kohei.sg@gmail.com>
2018-06-02 10:27:21 +0900

    Add kaminari pager

--- app/controllers/articles/drafts_controller.rb
+++ app/controllers/articles/drafts_controller.rb
@@ -1,6 +1,6 @@
 class Articles::DraftsController < ApplicationController
   def index
-    @articles = current_user.articles.draft.order(created_at: :desc)
+    @articles = current_user.articles.draft.order(created_at: :desc).page(params[:page])
     render "articles/index"
   end
 end
--- app/controllers/articles/published_controller.rb
+++ app/controllers/articles/published_controller.rb
@@ -1,6 +1,6 @@
 class Articles::PublishedController < ApplicationController
   def index
-    @articles = current_user.articles.published.order(created_at: :desc)
+    @articles = current_user.articles.published.order(created_at: :desc).page(params[:page])
     render "articles/index"
   end
 end
