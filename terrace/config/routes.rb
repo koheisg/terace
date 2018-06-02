e0aa80776e3fa107a0d226674329e2eb08c5609f
Kohei Sugi <kohei.sg@gmail.com>
2018-06-02 19:59:24 +0900

    Redirect pagenation

--- terrace/config/routes.rb
+++ terrace/config/routes.rb
@@ -2,5 +2,5 @@ Terrace::Engine.routes.draw do
   root to: 'articles#index'
 
   get '/:page', to: 'pages#index', page: /page\d+/
-  get '/:permalink', to: 'articles#show', permalink: /[^\s]+/
+  get '/:permalink', to: 'articles#show', permalink: /[^\s]+/, as: :article
 end
