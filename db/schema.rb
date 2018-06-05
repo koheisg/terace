5b8a468460077eca37ac6cb855a4681a82bab208
Kohei Sugi <kohei.sg@gmail.com>
2018-06-06 01:39:13 +0900

    Add migration

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_05_235655) do
+ActiveRecord::Schema.define(version: 2018_06_05_162955) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2018_03_05_235655) do
     t.string "permalink"
     t.datetime "published_at"
     t.datetime "modified_at"
-    t.index ["permalink", "user_id"], name: "index_articles_on_permalink_and_user_id", unique: true
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
