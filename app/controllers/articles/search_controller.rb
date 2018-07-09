d55db7aebb085b620a26131ad1cd35675301bb8c
Kohei SUGI <kohei.sg@gmail.com>
2018-07-09 21:21:18 +0900

    Tweek design for articles

--- app/controllers/articles/search_controller.rb
+++ app/controllers/articles/search_controller.rb
@@ -2,6 +2,7 @@ class Articles::SearchController < ApplicationController
   def index
     @q = Article::Search.new(search_params)
     @articles = @q.query(current_user.articles.order(created_at: :desc)).page(params[:page])
+    render 'articles/index'
   end
 
   def search_params
