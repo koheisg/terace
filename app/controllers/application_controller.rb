c12f4fb6bfd9f5202c38a6091f3d273e824c153b
Kohei SUGI <kohei.sg@gmail.com>
2018-07-09 19:22:51 +0900

    Switch login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   include Authenticatable
   protect_from_forgery with: :exception
   before_action :verify_user
-  helper_method :current_user
+  helper_method :current_user, :login?
 end
