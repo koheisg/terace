221388ccc2e00f0e8e6fc71978543d037fc4fbf9
Kohei SUGI <kohei.sg@gmail.com>
2018-07-10 09:58:12 +0900

    Apply domain

--- terrace/app/controllers/terrace/articles_controller.rb
+++ terrace/app/controllers/terrace/articles_controller.rb
@@ -3,13 +3,16 @@ require_dependency "terrace/application_controller"
 module Terrace
   class ArticlesController < ApplicationController
     def index
-      @user = ::User.find_by!(name: request.subdomain)
-      @articles = @user.articles.published.order(created_at: :desc).page(params[:page]).per(5)
+      @articles = site.articles.published.order(created_at: :desc).page(params[:page]).per(5)
     end
 
     def show
-      @user = ::User.find_by!(name: request.subdomain)
-      @article = ::Article.published.find_by!(permalink: params[:permalink])
+      @article = site.articles.published.find_by!(permalink: params[:permalink])
     end
+
+    private
+      def site
+        @site ||= ::Site.find_by!(domain: request.host)
+      end
   end
 end
