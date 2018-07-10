dd2e03f308842a994a1ca0ef3812bac3c157f5b9
Kohei SUGI <kohei.sg@gmail.com>
2018-07-10 23:38:17 +0900

    Make has_many through

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,4 @@
 class Site < ApplicationRecord
   has_many :articles
+  has_many :users, through: :user_sites
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :articles
+  has_many :user_sites
+  has_many :sites, through: :user_sites
 end
