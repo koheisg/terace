a4f5fe651272d0767957fa4e9d81b58d87360340
Kohei SUGI <kohei.sg@gmail.com>
2018-07-10 23:34:14 +0900

    Exec `$ rails g model user_site user:references site:references`

---
+++ db/migrate/20180710143343_create_user_sites.rb
@@ -0,0 +1,10 @@
+class CreateUserSites < ActiveRecord::Migration[6.0]
+  def change
+    create_table :user_sites do |t|
+      t.references :user, foreign_key: true
+      t.references :site, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
