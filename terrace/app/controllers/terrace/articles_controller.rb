49aef9005bad2156b640ca3bb65fef17584782ca
Kohei SUGI <kohei.sg@gmail.com>
2018-07-25 01:14:42 +0900

    Fix s/host/host_with_port/

--- terrace/app/controllers/terrace/articles_controller.rb
+++ terrace/app/controllers/terrace/articles_controller.rb
@@ -12,7 +12,7 @@ module Terrace
 
     private
       def site
-        @site ||= ::Site.find_by!(domain: request.host)
+        @site ||= ::Site.find_by!(domain: request.host_with_port)
       end
   end
 end
