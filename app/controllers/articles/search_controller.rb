2779c44fa80bf0ab279d45c5b553cd2d58272e33
Kohei SUGI <kohei.sg@gmail.com>
2018-07-25 02:30:54 +0900

    Make disable view unless current_user have the site

--- app/controllers/articles/search_controller.rb
+++ app/controllers/articles/search_controller.rb
@@ -1,6 +1,7 @@
 class Articles::SearchController < ApplicationController
   def index
     @q = Article::Search.new(search_params)
+    @q.user = current_user
     @articles = @q.query(current_user.articles.includes(:site).order(created_at: :desc)).page(params[:page])
     render 'articles/index'
   end
--- app/models/article/search.rb
+++ app/models/article/search.rb
@@ -1,12 +1,27 @@
 class Article::Search
   include ActiveModel::Model
-  attr_accessor :title, :state, :site
+  attr_accessor :title, :state, :site, :user
 
   def query(articles)
     res = articles
-    res = res.where(state: Article.states[state]) if state
-    res = res.where("articles.title LIKE ?", "%#{title}%") if title
-    res = res.where(site_id: site) if site
+    res = state_condition(res)
+    res = title_condition(res)
+    res = site_condition(res)
     res
+  end
+
+  def state_condition(query)
+    return query.where(state: Article.states[state]) if state
+    query
+  end
+
+  def title_condition(query)
+    return query.where("articles.title LIKE ?", "%#{title}%") if title
+    query
+  end
+
+  def site_condition(query)
+    return query.where(site_id: site) if site && user.sites.exists?(site)
+    query
   end
 end
