82d9b6afe7a68bd78d795cc7d850243d874c82f4
Kohei Sugi <kohei.sg@gmail.com>
2019-03-05 01:45:27 +0900

    Fix style

--- terrace/config/routes.rb
+++ terrace/config/routes.rb
@@ -1,6 +1,6 @@
 Terrace::Engine.routes.draw do
   root to: 'articles#index'
-  get :feed, to: 'rss#index', defaults: { format: :rss  }
+  get :feed, to: 'rss#index', defaults: { format: :rss }
 
   get '/:page', to: 'pages#index', page: /page\d+/
   get '/:permalink', to: 'articles#show', permalink: /[^\s]+/, as: :article
