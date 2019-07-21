46631c93c8638d3b196bb9b502a1e05f7fabaa95
Kohei SUGI <kohei.sg@gmail.com>
2019-07-22 01:50:17 +0900

    Fix server error

--- app/controllers/articles/drafts_controller.rb
+++ app/controllers/articles/drafts_controller.rb
@@ -1,5 +1,6 @@
 class Articles::DraftsController < ApplicationController
   def index
+    @sites = current_user.sites
     @articles = current_user.articles.includes(:site).draft.order(created_at: :desc).page(params[:page])
     render "articles/index"
   end
--- app/controllers/articles/published_controller.rb
+++ app/controllers/articles/published_controller.rb
@@ -1,5 +1,6 @@
 class Articles::PublishedController < ApplicationController
   def index
+    @sites = current_user.sites
     @articles = current_user.articles.includes(:site).published.order(created_at: :desc).page(params[:page])
     render "articles/index"
   end
