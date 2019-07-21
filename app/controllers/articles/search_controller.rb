81d8608d76357aaf4a864329014f14b148fc490b
Kohei SUGI <kohei.sg@gmail.com>
2019-07-22 01:30:10 +0900

    Fix search controller

--- app/controllers/articles/search_controller.rb
+++ app/controllers/articles/search_controller.rb
@@ -1,10 +1,13 @@
 class Articles::SearchController < ApplicationController
   def index
+    @sites = current_user.sites
     @q = Article::Search.new(search_params)
     @q.user = current_user
     @articles = @q.query(current_user.articles.includes(:site).order(created_at: :desc)).page(params[:page])
     render 'articles/index'
   end
+
+  private
 
   def search_params
     params.permit(:title, :state, :site)
