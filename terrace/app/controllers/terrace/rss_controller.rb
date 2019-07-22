39757e063d964e555d2b8b29b20b3a5e6de55a02
Kohei SUGI <kohei.sg@gmail.com>
2019-07-22 11:13:47 +0900

    Fix feed

--- terrace/app/controllers/terrace/rss_controller.rb
+++ terrace/app/controllers/terrace/rss_controller.rb
@@ -4,8 +4,8 @@ module Terrace
   class RssController < ApplicationController
     layout false
     def index
-      @site = ::Site.find_by!(name: request.subdomain)
-      @articles = site.articles.published.order(created_at: :desc).limit(10)
+      @site = ::Site.find_by!(domain: request.host)
+      @articles = @site.articles.published.order(created_at: :desc).limit(10)
     end
   end
 end
