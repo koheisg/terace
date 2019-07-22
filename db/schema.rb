f02134790daf85ad5789515d23030e2a3e8ddcd8
Kohei SUGI <kohei.sg@gmail.com>
2019-07-22 10:11:52 +0900

    $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_10_143343) do
+ActiveRecord::Schema.define(version: 2019_07_22_011019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,8 @@ ActiveRecord::Schema.define(version: 2018_07_10_143343) do
     t.string "domain"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "layout"
   end
 
   create_table "taggings", force: :cascade do |t|
