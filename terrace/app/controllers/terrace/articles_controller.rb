5cb50a4ca930420633241af6e8d7430c0332c593
Kohei SUGI <kohei.sg@gmail.com>
2019-07-25 01:26:55 +0900

    Fix layout for css

--- terrace/app/controllers/terrace/articles_controller.rb
+++ terrace/app/controllers/terrace/articles_controller.rb
@@ -2,6 +2,7 @@ require_dependency "terrace/application_controller"
 
 module Terrace
   class ArticlesController < ApplicationController
+    layout :site_layout
     def index
       @articles = current_site.articles.published.order(created_at: :desc).page(params[:page]).per(5)
     end
@@ -9,5 +10,10 @@ module Terrace
     def show
       @article = current_site.articles.published.find_by!(permalink: params[:permalink])
     end
+
+    private
+      def site_layout
+        "terrace/#{current_site.layout}"
+      end
   end
 end
