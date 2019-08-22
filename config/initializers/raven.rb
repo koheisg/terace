30499aff317d686e432a29b20685ad942f25b55f
Kohei SUGI <kohei.sg@gmail.com>
2019-08-23 01:53:02 +0900

    Fix Rails.application.credentials method

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,4 +1,4 @@
 Raven.configure do |config|
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.dsn = Rails.application.credentials[Rails.env.to_sym][:sentry_dsn]
+  config.dsn = Rails.application.credentials[:sentry_dsn]
 end
