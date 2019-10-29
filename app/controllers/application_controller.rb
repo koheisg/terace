4da2c0e03d52cc6999dc81d09bff37e2cbdc06d3
Kohei SUGI <kohei.sg@gmail.com>
2019-10-29 22:48:07 +0900

    Add current_site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   include Authorizable
   protect_from_forgery with: :exception
   before_action :verify_user
+  helper_method :current_site
   helper_method :current_user, :login?, :admin?
 
   before_action :set_raven_context
@@ -12,5 +13,9 @@ class ApplicationController < ActionController::Base
   def set_raven_context
     Raven.user_context(id: session[:user_id]) # or anything else in session
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
+
+  def current_site
+    Site.find_by(id: session[:site_id])
   end
 end
