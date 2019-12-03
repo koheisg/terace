b484d8f8f67e044cf919264995cc64a00c528aa7
Kohei SUGI <kohei.sg@gmail.com>
2019-12-03 09:34:06 +0900

    Add article_test

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -7,7 +7,73 @@ class ArticleTest < ActiveSupport::TestCase
     @article = Article.new(user: user, site: site)
   end
 
-  test "save article" do
+  test 'invalid when permalink is nil and state is shipped' do
+    @article.permalink = nil
+    @article.state = :shipped
+    assert @article.invalid?
+    assert_equal ["Permalink can't be blank"], @article.errors.full_messages
+  end
+
+  test 'valid when permalink is nil and state is shipped' do
+    @article.permalink = nil
+    @article.state = :draft
+    assert @article.valid?
+  end
+
+  test 'published_at and modified_at when save article as draft' do
+    @article.state = :draft
+    assert @article.save
+    assert_nil @article.published_at
+    assert_nil @article.modified_at
+  end
+
+  test 'published_at and modified_at when save article as shipped' do
+    @article.permalink = 'test'
+    @article.state = :shipped
+    assert @article.save
+    assert @article.published_at
+    assert @article.modified_at
+  end
+
+  test 'published_at and modified_at when update article from draft to draft' do
+    @article.state = :draft
+    @article.save
+    @article.permalink = 'test'
+    assert @article.save
+
+    assert_nil @article.published_at
+    assert_nil @article.modified_at
+  end
+
+  test 'published_at and modified_at when update article from draft to shipped' do
+    @article.state = :draft
+    assert @article.save
+    @article.permalink = 'test'
+    assert @article.shipped!
+
+    assert @article.published_at
+    assert @article.modified_at
+  end
+
+  test 'published_at and modified_at when update article from shipped to shipped' do
+    @article.state = :shipped
+    @article.permalink = 'test'
+    assert @article.save
+    assert @article.published_at
+    assert @article.modified_at
+
+    assert_changes ->{ @article.modified_at } do
+      @article.permalink = 'test1'
+      assert @article.save
+    end
+
+    assert_no_changes ->{ @article.published_at } do
+      @article.permalink = 'test2'
+      assert @article.save
+    end
+  end
+
+  test "audits when save article" do
     assert @article.save
     assert_equal 'create', @article.audits.last.action
     assert_equal 1, @article.audits.size
