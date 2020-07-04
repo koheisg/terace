d3ea9179b6638fb84a9de1a9fe1da0db4ebf4d71
Kohei SUGI <kohei.sg@gmail.com>
2020-07-04 19:25:24 +0900

    Fix database url

--- bin/data_from_production.sh
+++ bin/data_from_production.sh
@@ -1,6 +1,6 @@
 PRODUCTION_DATABASE_URL=`heroku config:get DATABASE_URL -a terace`
 pg_dump -a -d  $PRODUCTION_DATABASE_URL > hoge.sql
 ./bin/rails db:reset
-psql -d postgres://postgres@db:54320/terace_development -f hoge.sql
+psql -d postgres://postgres@0.0.0.0:54320/terace_development -f hoge.sql
 rm hoge.sql
 ./bin/rails r 'User.all.each {|u| u.update(password: "pasword")}; Site.all.each {|s| s.domain = s.domain.split(".").first + ".lvh.me"; s.save }'
