a523565bccf9bff1fe6810738869ef341cb8ab80
Kohei SUGI <kohei.sg@gmail.com>
2020-07-07 14:51:37 +0900

    Delete not used files

--- terrace/config/routes.rb
+++ terrace/config/routes.rb
@@ -1,6 +1,4 @@
 Terrace::Engine.routes.draw do
   get :feed, to: 'rss#index', defaults: { format: :rss }
-
-  get '/:page', to: 'pages#index', page: /page\d+/
   get '/:permalink', to: 'permalinks#show', permalink: /.*/, as: :article
 end
