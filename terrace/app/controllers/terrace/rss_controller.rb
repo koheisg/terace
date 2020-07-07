83678867bfde8fdd73ac4dde669d91194e2a876c
Kohei SUGI <kohei.sg@gmail.com>
2020-07-08 00:18:37 +0900

    Fix feed

--- terrace/app/controllers/terrace/rss_controller.rb
+++ terrace/app/controllers/terrace/rss_controller.rb
@@ -4,7 +4,7 @@ module Terrace
   class RssController < ApplicationController
     layout false
     def index
-      @permalinks = current_site.permalinks.includes(:permalinkable).published.order(created_at: :desc).limit(10)
+      @permalinks = current_site.permalinks.includes(:permalinkable).article_types.published.order(created_at: :desc).limit(10)
     end
   end
 end
