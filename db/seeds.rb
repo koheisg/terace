4551c519ba1c1a32cd8030f9793fe1c56cedf071
Kohei SUGI <kohei.sg@gmail.com>
2020-07-11 01:49:30 +0900

    Improve seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-User.create({name: "koheisg", password: "password", admin: true})
+users = User.create([{name: "koheisg", password: "password", admin: true}])
+users.first.sites.create(name: 'koheisg site', domain: 'koheisg.lvh.me')
