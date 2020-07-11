fee4668de95fbe03d8a3582871ff63d8445daccc
Kohei SUGI <kohei.sg@gmail.com>
2020-07-11 12:53:13 +0900

    Fix typo

--- bin/data_from_production.sh
+++ bin/data_from_production.sh
@@ -3,4 +3,4 @@ pg_dump -a -d  $PRODUCTION_DATABASE_URL > hoge.sql
 ./bin/rails db:schema:load
 psql -d postgres://postgres:postgres@0.0.0.0:54320/terace_development -f hoge.sql
 rm hoge.sql
-./bin/rails r 'User.all.each {|u| u.update(password: "pasword")}; Site.all.each {|s| s.domain = s.domain.split(".").first + ".lvh.me"; s.save }'
+./bin/rails r 'User.all.each {|u| u.update(password: "password")}; Site.all.each {|s| s.domain = s.domain.split(".").first + ".lvh.me"; s.save }'
