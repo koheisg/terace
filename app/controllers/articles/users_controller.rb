e984c6c4610301d32426628caf50a1c9d5104e9e
Kohei SUGI <kohei.sg@gmail.com>
2020-07-26 23:03:24 +0900

    Delete not used controller

--- app/controllers/articles/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class Articles::UsersController < ApplicationController
-  skip_before_action :verify_user
-
-  def index
-    @user = User.find_by!(name: request.subdomain)
-    @articles = @user.articles.published
-  end
-end
