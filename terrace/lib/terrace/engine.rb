e78be2a31320163e95663936174aca223fb425fb
Kohei SUGI <kohei.sg@gmail.com>
2020-09-22 04:15:43 +0900

    tmp

--- terrace/lib/terrace/engine.rb
+++ terrace/lib/terrace/engine.rb
@@ -1,5 +1,7 @@
 module Terrace
   class Engine < ::Rails::Engine
     isolate_namespace Terrace
+
+    config.exceptions_app = ActionDispatch::PublicExceptions.new(Terrace::Engine.root.join('public'))
   end
 end
