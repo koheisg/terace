bbd8c865e73ac84ff563c538bdf45c7041548fb9
Kohei SUGI <kohei.sg@gmail.com>
2021-05-01 18:39:29 +0900

    bin/update

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_164654) do
+ActiveRecord::Schema.define(version: 2020_07_27_174340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,14 @@ ActiveRecord::Schema.define(version: 2020_07_13_164654) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "permalinks_count"
     t.index ["site_id"], name: "index_categories_on_site_id"
+  end
+
+  create_table "feeds", force: :cascade do |t|
+    t.string "archiveable_type", null: false
+    t.bigint "archiveable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["archiveable_type", "archiveable_id"], name: "index_feeds_on_archiveable_type_and_archiveable_id"
   end
 
   create_table "pages", force: :cascade do |t|
