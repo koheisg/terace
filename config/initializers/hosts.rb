4a1a5cac8cfeb32c7e02aa46c0829bd261b3de07
Kohei SUGI <kohei.sg@gmail.com>
2021-05-04 02:32:15 +0900

    Add domain

--- config/initializers/hosts.rb
+++ config/initializers/hosts.rb
@@ -1,3 +1,6 @@
 Rails.application.configure do
-  config.hosts = ".#{ENV['MAIN_DOMEIN']}"
+  config.hosts << ".#{ENV['MAIN_DOMEIN']}"
+  config.hosts << "koheisg.dreamin.cc"
+  config.hosts << "dev.dreamin.cc"
+  config.hosts << "www.forward-cc.com"
 end
