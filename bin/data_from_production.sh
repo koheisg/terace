717d7daa1e268f6c517c9d82f2963d87bfd07c81
Kohei SUGI <kohei.sg@gmail.com>
2021-08-13 22:07:00 +0900

    Delete docker

--- bin/data_from_production.sh
+++ bin/data_from_production.sh
@@ -1,6 +1,6 @@
 PRODUCTION_DATABASE_URL=`heroku config:get DATABASE_URL -a terace`
 pg_dump -a -d  $PRODUCTION_DATABASE_URL > hoge.sql
 ./bin/rails db:schema:load
-psql -d postgres://postgres:postgres@0.0.0.0:54320/terace_development -f hoge.sql
+psql -d postgres://localhost:5432/terace_development -f hoge.sql
 rm hoge.sql
 ./bin/rails r 'User.all.each {|u| u.update(password: "password")}; Site.all.each {|s| s.domain = s.domain.split(".").first + ".lvh.me"; s.save }'
