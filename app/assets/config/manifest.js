d89d36c6b83d0e12b132cca763f05e52dfc753a2
Kohei SUGI <kohei.sg@gmail.com>
2021-09-11 17:24:18 +0900

    rails css:install:tailwind

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,9 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
+
 
 //= link desk/application.css
 //= link terrace/application.css
 //= link terrace/dev/application.css
 //= link terrace/application.js
+//= link_tree ../builds
