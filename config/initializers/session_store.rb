7baca59d1740f5fcc5add31202457860e26a468d
Kohei SUGI <koheisg@hey.com>
2023-05-21 14:25:16 +0900

    Fix redis

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,1 @@
-if Rails.env.production?
-  Rails.application.config.session_store :redis_store, servers: ENV['REDIS_URL'], expire_after: 1.week
-else
-  Rails.application.config.session_store :cookie_store
-end
+Rails.application.config.session_store :redis_store, servers: ENV['REDIS_URL'], expire_after: 1.week
